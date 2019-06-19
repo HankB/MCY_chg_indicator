9cb2d0baa8c08090d54fc39c50fa9c5527cd2af8
Hank Barta <hbarta@gmail.com>
2019-06-19 16:22:26 -0500

    refactor analog read

--- MCY_chg_indicator.ino
+++ MCY_chg_indicator.ino
@@ -9,7 +9,7 @@
   http://www.arduino.cc/en/Tutorial/AnalogInput
 
   Monitor an analog input and illuminate an RGB LED to indicate evaluation of
-  the charging voltage. Input volgate is scaled 1:3 so that 15V from the 
+  the charging voltage. Input volgate is scaled 1:3 so that 15V from the
   charging system would present 5.0V at the analog input.
 
   See https://docs.google.com/document/d/1oGInpiPI6TI1JvXcmjz_O_Tvbv5o_kn_NtnWW1Aht5o/edit?usp=sharing
@@ -27,7 +27,7 @@ static const uint16_t spread = good - bad;
 
 // I really wish Arduino didn't hide the signatures for library finctions like pinMode()
 static const int sensorPin =   A0;   // select the input pin for the potentiometer
-static const int redPin =      11;   // select the pin for the LED
+static const int redPin =      9;   // select the pin for the LED
 static const int greenpin =    10;
 
 static uint16_t sensorValue = 0;    // variable to store the value coming from the sensor
@@ -36,11 +36,7 @@ const uint16_t red_pct = 50;
 const uint16_t green_pct = 50;
 const uint16_t max_DAC   = 255;
 
-static const uint16_t analog_repeat =        8;  // consedutive analog readings to sum to reduce noise
-static const uint16_t analog_repeat_delay =  4;  // delay in ms between consecutive analog readings
-static uint16_t readings[analog_repeat];
-
-#define SERIAL_OUT  0
+#define SERIAL_OUT  1
 
 void setup() {
   // declare the ledPin as an OUTPUT:
@@ -51,47 +47,58 @@ void setup() {
 #endif //SERIAL_OUT
 }
 
-void loop() {
-  // read the value from the sensor:
-  sensorValue = 0;
-  for(int i=0; i<analog_repeat; i++) {
+uint16_t readVoltage(void) {
+  static const uint16_t analog_repeat =        8;  // consedutive analog readings to sum to reduce noise
+  static const uint16_t analog_repeat_delay =  4;  // delay in ms between consecutive analog readings
+  static uint16_t readings[analog_repeat];
+  static uint16_t value;
+
+  for (int i = 0; i < analog_repeat; i++) {
     readings[i] = analogRead(sensorPin);
-    sensorValue += readings[i];
+    value += readings[i];
     delay(analog_repeat_delay);
   }
+  value = (value + analog_repeat / 2) / analog_repeat;  // divide with rounding
+
 #if SERIAL_OUT
-  for(int i=0; i<analog_repeat; i++) {
+  for (int i = 0; i < analog_repeat; i++) {
     Serial.print(readings[i]);
     Serial.print(" ");
   }
-    Serial.print(" ");
-    Serial.print(sensorValue);
+  Serial.print(" ");
+  Serial.print(value);
 #endif //SERIAL_OUT
-  sensorValue = (sensorValue + analog_repeat/2)/analog_repeat;
+  return value;
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = readVoltage();
+
 #if SERIAL_OUT
-    Serial.print(" rounded ");
-    Serial.print(sensorValue);
+  Serial.print(" rounded ");
+  Serial.print(sensorValue);
 #endif //SERIAL_OUT
-  if(sensorValue >= good) {
-    analogWrite(greenpin, (max_DAC*green_pct)/100);
+  if (sensorValue >= good) {
+    analogWrite(greenpin, (max_DAC * green_pct) / 100);
     analogWrite(redPin, 0);
 #if SERIAL_OUT
     Serial.print(" good ");
     Serial.println(sensorValue);
 #endif //SERIAL_OUT
-    }
-  else if(sensorValue < bad) {
+  }
+  else if (sensorValue < bad) {
     analogWrite(greenpin, 0);
-    analogWrite(redPin, (max_DAC*red_pct)/100);
+    analogWrite(redPin, (max_DAC * red_pct) / 100);
 #if SERIAL_OUT
     Serial.print(" bad  ");
     Serial.println(sensorValue);
 #endif //SERIAL_OUT
   }
   else {
-    uint16_t blend = (sensorValue-bad)*100/spread;  // calculate blend percent
-    uint16_t green = blend*max_DAC/100*green_pct/100;
-    uint16_t red = (100-blend)*max_DAC/100*red_pct/100;
+    uint16_t blend = (sensorValue - bad) * 100 / spread; // calculate blend percent
+    uint16_t green = blend * max_DAC / 100 * green_pct / 100;
+    uint16_t red = (100 - blend) * max_DAC / 100 * red_pct / 100;
     analogWrite(greenpin, green);
     analogWrite(redPin, red);
 #if SERIAL_OUT
@@ -99,7 +106,7 @@ void loop() {
     Serial.print(" blend ");  Serial.print(blend);
     Serial.print(" green ");  Serial.print(green);
     Serial.print(" red ");    Serial.print(red);
-    Serial.print(" sum ");    Serial.println(green+red);
+    Serial.print(" sum ");    Serial.println(green + red);
 #endif //SERIAL_OUT
   }
 #if SERIAL_OUT
